ddd0e567cc44897aa0a15e82c7a440e4ff8df47b
hikiray <satalomaksimka12345678910@gmail.com>
2024-03-05 18:55:38 +0700

    Обратный ход ура

--- GaussMethod.h
+++ GaussMethod.h
@@ -24,9 +24,25 @@ public:
                 }
             }
         }
+        //Прямой ход
+        for (int k = rows- 1; k > -1; k--)
+        {
+            for (int i = rows; i > -1; i--)
+                matrixA[k][i] = matrixA[k][i] / matrixA[k][k];
+            for (int i = k - 1; i > -1; i--)
+            {
+                float K = matrixA[i][k] / matrixA[k][k];
+                for (int j = rows; j > -1; j--)
+                    matrixA[i][j] = matrixA[i][j] - matrixA[k][j] * K;
+            }
+        }
+        //Обратный ход
+        PrintMatrix(matrixA,rows);
+    }
+    static void PrintMatrix(float** matrix,int rows){
         for(int i = 0; i < rows;i++ ){
             for (int j = 0; j < rows+1; ++j) {
-                cout << matrixA[i][j] << "  ";
+                cout << matrix[i][j] << "  ";
             }
             cout << endl;
         }
