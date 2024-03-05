fe972bcaa83c48c1df1f19fcb0629caf5d4d0a11
hikiray <satalomaksimka12345678910@gmail.com>
2024-03-05 18:32:22 +0700

    Прямой ход сделан

--- GaussMethod.h
+++ GaussMethod.h
@@ -19,27 +19,33 @@ private:
 //        };
 public:
     static void StartMethod(float **matrixA, float **matrixB, int rows) {
-        float **matrixCloneA = matrixA;
-        for(int i = 0; i < rows-1;i++){ // строка первая
+        for(int i = 0; i < rows;i++ ){
+            for (int j = 0; j < rows+1; ++j) {
+                cout << matrixA[i][j] << "  ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+        for(int i = 0; i < rows;i++){ // строка первая
             for(int j = 0; j < rows; j++){ // проверка на нулевой элемент
                 if(matrixA[j][i] != 0){
                     break;
                 }
             }
+            float divider = matrixA[i][i];
+            for(int j = 0; j < rows+1; j++){
+                float tmp = matrixA[i][j];
+                matrixA[i][j] = matrixA[i][j]/ divider;
+            }
             for (int j = i+1; j < rows; ++j) {// следующую строку превращаем
-                float cup = matrixA[j][i];
-                for(int k = i;k < rows-1; k++){
-//                    float numberTwo = -matrixA[j][k];
-                    float numberTwo = -cup;
-                    float numberOne = matrixA[0][i];
-                    float tmp = numberOne * (numberTwo/numberOne);
-                    matrixA[j][k] += tmp;
+                float tmp = -matrixA[j][i]/matrixA[i][i];
+                for(int k = i;k < rows+1; k++){
+                    matrixA[j][k] += matrixA[i][k] * tmp;
                 }
             }
         }
-
         for(int i = 0; i < rows;i++ ){
-            for (int j = 0; j < rows; ++j) {
+            for (int j = 0; j < rows+1; ++j) {
                 cout << matrixA[i][j] << "  ";
             }
             cout << endl;
--- main.cpp
+++ main.cpp
@@ -3,26 +3,30 @@
 int main() {
     float matrixCreate[3][3]
         { {3,5,1},
-          {1.799999,3,7,},
+          {1.799999,3,7},
           {8,1,1}
         };
-//    float matrixB[3][1]
-//        {
-//            {12},
-//            {13.599998},
-//            {18},
-//        };
+    float matrixB[3][1]
+        {
+            {12},
+            {13.599998},
+            {18},
+        };
+    float matrixBarray[]{12,13.599998,18};
     int rows = 3;
     int cols = 3;
     float** matrixA;
-    matrixA = static_cast<float **>(calloc(rows, sizeof(float *)));
+    matrixA = static_cast<float **>(calloc(rows+1, sizeof(float *)));
     for(int i = 0; i < rows; i++){
-        matrixA[i] = static_cast<float *>(calloc(cols, sizeof(float)));
+        matrixA[i] = static_cast<float *>(calloc(cols+1, sizeof(float)));
     }
     for(int i = 0 ; i < rows; i++){
         for (int j = 0; j < rows; ++j) {
             matrixA[i][j] = matrixCreate[i][j];
         }
+    }
+    for(int i = 0;i < rows; i++){
+        matrixA[i][rows] = matrixBarray[i];
     }
 //    int rows = ...;
 //    int cols = ...;
@@ -32,7 +36,13 @@ int main() {
 //        m[i] = calloc(cols, sizeof(float));
 //    }
 ///* как-то заполняете значения */
-//    float res = det(m, rows, cols);
+////    float res = det(m, rows, cols);
+//    for(int i = 0; i < rows;i++ ){
+//        for (int j = 0; j < rows+1; ++j) {
+//            cout << matrixA[i][j] << "  ";
+//        }
+//        cout << endl;
+//    }
     GaussMethod::StartMethod(matrixA,matrixA,3);
     return 0;
 }
